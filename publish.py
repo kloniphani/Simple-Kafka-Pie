44ccb4cc2ef58f69b001a55e5330ed9a4c64cf22
Kloniphani Maxakadzi <kloniphani@gmail.com>
2021-04-01 14:11:58 +0200

    fixed byte string

--- publish.py
+++ publish.py
@@ -13,5 +13,5 @@ topic = client.topics[b'bde']
 
 with topic.get_sync_producer() as producer:
     while True:
-        producer.produce(bytes(string"temperature:{0}".format(temperature)'utf-8'))
+        producer.produce(bytes("temperature:{0}".format(temperature)'utf-8'))
         print("Temp: {0:0.1f} C  Humidity: {1:0.1f} %".format(temperature, humidity))
