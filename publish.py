88c5bc096f2ff82999254cc40fe5db43d22922e7
Kloniphani Maxakadzi <kloniphani@gmail.com>
2021-04-01 13:18:26 +0200

    fixed xreange error

--- publish.py
+++ publish.py
@@ -14,17 +14,6 @@ def acked(err, msg):
 
 p = Producer({'bootstrap.servers': '192.168.74.140 :9092'})
 
-try:
-    for val in xrange(1, 1000):
-        p.produce('mytopic', 'myvalue #{0}'
-                  .format(val), callback=acked)
-        p.poll(0.5)
-
-except KeyboardInterrupt:
-    pass
-
-p.flush(30)
-
 while True:
 
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
