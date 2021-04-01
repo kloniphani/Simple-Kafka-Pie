4173acd42ca7c9fed6edd8663ace50296d3e70a6
Kloniphani Maxakadzi <kloniphani@gmail.com>
2021-04-01 13:55:38 +0200

    test 1

--- consume.py
+++ consume.py
@@ -15,36 +15,13 @@ sense.set_pixel(4, 5, red)
 
 sense.clear(purple)
 
-from confluent_kafka import Consumer, KafkaError
+from pykafka import KafkaClient
 
-settings = {
-    'bootstrap.servers': '192.168.0.161:9092',
-    'group.id': 'mygroup',
-    'client.id': 'client-1',
-    'enable.auto.commit': True,
-    'session.timeout.ms': 6000,
-    'default.topic.config': {'auto.offset.reset': 'smallest'}
-}
+client = KafkaClient(hosts="192.168.0.161")
+client.topics
+topic = client.topics['bde']
 
-c = Consumer(settings)
-
-c.subscribe(['bde'])
-
-try:
-    while True:
-        msg = c.poll(0.1)
-        if msg is None:
-            continue
-        elif not msg.error():
-            print('Received message: {0}'.format(msg.value()))
-        elif msg.error().code() == KafkaError._PARTITION_EOF:
-            print('End of partition reached {0}/{1}'
-                  .format(msg.topic(), msg.partition()))
-        else:
-            print('Error occured: {0}'.format(msg.error().str()))
-
-except KeyboardInterrupt:
-    pass
-
-finally:
-    c.close()
+consumer = topic.get_simple_consumer()
+for message in consumer:
+    if message is not None:
+        print(message.offset, message.value)
