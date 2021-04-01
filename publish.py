e31151afe02367959b0e0d5f694c04a73c2a1a4f
Kloniphani Maxakadzi <kloniphani@gmail.com>
2021-04-01 14:06:34 +0200

    added the port

--- publish.py
+++ publish.py
@@ -7,9 +7,9 @@ humidity, temperature = Adafruit_DHT.read_retry(11, 4)
 
 from pykafka import KafkaClient
 
-client = KafkaClient(hosts="192.168.0.161,")
+client = KafkaClient(hosts="192.168.0.161:9092")
 client.topics
-topic = client.topics['bde']
+topic = client.topics[b'bde']
 
 with topic.get_sync_producer() as producer:
     while True:
