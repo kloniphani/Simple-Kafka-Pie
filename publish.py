ca1414826983f42e19b35677850f73931e3024a5
Kloniphani Maxakadzi <kloniphani@gmail.com>
2021-04-01 13:59:29 +0200

    printing

--- publish.py
+++ publish.py
@@ -2,9 +2,11 @@
 import sys
 import Adafruit_DHT
 import time
+from pykafka import KafkaClient
 
 humidity, temperature = Adafruit_DHT.read_retry(11, 4)
 
 with topic.get_sync_producer() as producer:
     while True:
         producer.produce("temperature:" + temperature)
+        print("Temp: {0:0.1f} C  Humidity: {1:0.1f} %".format(temperature, humidity))
