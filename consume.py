1b74d5518bc42c35dcdab09755998615c6719f0d
Kloniphani Maxakadzi <kloniphani@gmail.com>
2021-04-01 12:22:39 +0200

    added new file for consume

---
+++ consume.py
@@ -0,0 +1,14 @@
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+red = (255, 0, 0)
+orange = (255, 165, 0)
+yellow = (255, 255, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+purple = (160, 32, 240)
+
+sense.set_pixel(4, 5, red)
+
+#sense.clear(purple)
