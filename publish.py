d808130dc08c1b65d32687db7fe4fa807c84e421
Kloniphani Maxakadzi <kloniphani@gmail.com>
2021-04-03 00:32:25 +0200

    soundSensor

--- publish.py
+++ publish.py
@@ -2,9 +2,24 @@
 import sys
 import Adafruit_DHT
 import time
+import RPi.GPIO as GPIO
 from pioneer.BMP180 import BMP180
 
 bmp = BMP180()
+
+#GPIO SETUP
+soundSensor = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(soundSensor, GPIO.IN)
+
+def callback(soundSensor):
+        if GPIO.input(soundSensor):
+                print "Sound Detected!"
+        else:
+                print "Sound Detected!"
+
+GPIO.add_event_detect(soundSensor, GPIO.BOTH, bouncetime=300)  # let us know when the pin goes HIGH or LOW
+GPIO.add_event_callback(soundSensor, callback)  # assign function to GPIO PIN, Run function on change
 
 while True:
     temp = bmp.read_temperature()
