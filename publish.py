93c9f37604af72f6c4e29fa9456739e6752dda8d
Kloniphani Maxakadzi <kloniphani@gmail.com>
2021-04-03 01:57:32 +0200

    tabbing

--- publish.py
+++ publish.py
@@ -41,11 +41,11 @@ while True:
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
     print("Temp: {0:0.1f} C  Humidity: {1:0.1f} %".format(temperature, humidity))
 
-	bus.write_byte_data(address,cmd,value)
-	value += 1
-	if value == 256:
-		value =0
-	print("AOUT:{0:0.1f}".format(%value))
+    bus.write_byte_data(address,cmd,value)
+    value += 1
+    if value == 256:
+        value =0
+    print("AOUT:{0:0.1f}".format(%value))
 
 from time import sleep
 from json import dumps
